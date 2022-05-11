d1ff600dc9e0bb90f147937198b6c940e620f4ba
Chapi777 <73992659+Chapi777@users.noreply.github.com>
2022-05-11 14:16:28 +0300

    Add files via upload

---
+++ YaEb2/YaEb2/Form4.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace YaEb2
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butFormatRTF = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.butGetRTF = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butFormatRTF
+            // 
+            this.butFormatRTF.Location = new System.Drawing.Point(12, 44);
+            this.butFormatRTF.Name = "butFormatRTF";
+            this.butFormatRTF.Size = new System.Drawing.Size(238, 35);
+            this.butFormatRTF.TabIndex = 0;
+            this.butFormatRTF.Text = "Листинг 6.31. Управление цветом и шрифтом в RichTextBox";
+            this.butFormatRTF.UseVisualStyleBackColor = true;
+            this.butFormatRTF.Click += new System.EventHandler(this.butFormatRTF_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(256, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(188, 35);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_LinkClicked);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(256, 85);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 35);
+            this.textBox1.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(256, 126);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(138, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Листинг 6.36. Вывод пустого текста в DateTimePicker";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(238, 37);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Листинг 6.37. Программное раскрытие списка DateTimePicker";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // butGetRTF
+            // 
+            this.butGetRTF.Location = new System.Drawing.Point(12, 4);
+            this.butGetRTF.Name = "butGetRTF";
+            this.butGetRTF.Size = new System.Drawing.Size(238, 35);
+            this.butGetRTF.TabIndex = 8;
+            this.butGetRTF.Text = "Листинг 6.30. Просмотр команд управления RTF";
+            this.butGetRTF.UseVisualStyleBackColor = true;
+            this.butGetRTF.Click += new System.EventHandler(this.butGetRTF_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(400, 126);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(22, 20);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "x";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(256, 44);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(188, 35);
+            this.richTextBox2.TabIndex = 10;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(450, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 24);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Проверка";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 212);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(238, 37);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Листинг 6.38. Создание полупрозрачной надписи и панели";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(451, 28);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel1.TabIndex = 13;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 255);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(238, 37);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Листинг 6.39. Установка свойств для LinkLabel";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 85);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(238, 37);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Листинг 6.34. Проверка на наличие полосы прокрутки в RichTextBox";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.butGetRTF);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.butFormatRTF);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butFormatRTF;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button butGetRTF;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
---
+++ YaEb2/YaEb2/Form4.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace YaEb2
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+            richTextBox1.AllowDrop = true;
+            this.richTextBox1.DragEnter +=
+            new System.Windows.Forms.DragEventHandler(
+            this.richTextBox2_DragEnter);
+            this.richTextBox1.DragDrop +=
+            new System.Windows.Forms.DragEventHandler(
+            this.richTextBox2_DragEnter);
+            RichTextBoxEx rboxex = new RichTextBoxEx();
+            rboxex.Parent = this;
+            rboxex.Top = 400;
+            richTextBox2.AllowDrop = true;
+        }
+        class RichTextBoxEx : RichTextBox
+        {
+            protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+            {
+                //Проверяем на нажатие Shift+Insert и Ctrl+V
+                if ((keyData & (Keys.Shift | Keys.Insert)) ==
+                (Keys.Shift | Keys.Insert)
+                || ((keyData & (Keys.Control | Keys.V)) ==
+                (Keys.Control | Keys.V)))
+                return true;
+                return base.ProcessCmdKey(ref msg, keyData);
+            }
+        }
+        private void richTextBox2_DragEnter(object sender,
+            System.Windows.Forms.DragEventArgs e)
+        {
+            if (((DragEventArgs)e).Data.GetDataPresent(DataFormats.Text))
+                ((DragEventArgs)e).Effect = DragDropEffects.Move;
+            else
+                ((DragEventArgs)e).Effect = DragDropEffects.None;
+        }
+        private void richTextBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            richTextBox1.LoadFile((String)e.Data.GetData("Text"),
+            System.Windows.Forms.RichTextBoxStreamType.RichText);
+        }
+
+        private void butFormatRTF_Click(object sender, EventArgs e)
+        {
+            // Устанавливаем фокус на richTextBox
+            richTextBox1.Focus();
+            // Устанавливаем цвет для выделенного текста
+            richTextBox1.SelectionColor = Color.Red;
+            // Устанавливаем шрифт
+            richTextBox1.SelectionFont = new Font("Courier", 10, FontStyle.Bold);
+        }
+
+        private void richTextBox1_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.CustomFormat = " ";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Устанавливаем фокус
+            dateTimePicker1.Focus();
+            // Посылаем команду
+            SendKeys.Send("{F4}");
+        }
+
+        private void butGetRTF_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = richTextBox1.Rtf;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+        }
+
+        private void richTextBox2_LinkClicked(object sender,
+            LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.FromArgb(60, 255, 192, 192);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://rusproject.narod.ru/");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            linkLabel1.Text = "На нашем сайте вы найдете дополнительную информацию";
+            linkLabel1.LinkArea = new LinkArea(3, 11);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Вертикальная прокрутка: " +
+            IsVertScrollPresent(richTextBox1).ToString() +
+               Environment.NewLine + "Горизонтальная прокрутка: " +
+            IsHorScrollPresent(richTextBox1).ToString();
+        }
+        [DllImport("User32.dll")]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        const int GWL_STYLE = -16;
+        const int WS_HSCROLL = 0x00100000;
+        const int WS_VSCROLL = 0x00200000;
+        // Проверка на наличие вертикальной прокрутки
+        bool IsVertScrollPresent(Control control)
+        {
+            int style = GetWindowLong(control.Handle, GWL_STYLE);
+            return (style & WS_VSCROLL) > 0;
+        }
+        // Проверка на наличие горизонтальной прокрутки
+        bool IsHorScrollPresent(Control control)
+        {
+            int style = GetWindowLong(control.Handle, GWL_STYLE);
+            return (style & WS_HSCROLL) > 0;
+        }
+    }
+}
